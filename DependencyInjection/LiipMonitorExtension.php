9821f485de00e772791623f1b60274526e876407
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-09-24 16:33:16 +0200

    make the configuration class more explicit

--- DependencyInjection/LiipMonitorExtension.php
+++ DependencyInjection/LiipMonitorExtension.php
@@ -23,76 +23,51 @@ class LiipMonitorExtension extends Extension
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
         foreach ($config['checks'] as $check => $values) {
-            $serviceId = $this->getAlias().'.check.'.$check;
-            if ($container->hasDefinition($serviceId)) {
-                $service = $container->getDefinition($serviceId);
+            if (empty($values)) {
+                continue;
             }
 
+            $serviceId = $this->getAlias().'.check.'.$check;
+            $service = $container->getDefinition($serviceId);
             switch ($check) {
                 case 'custom_error_pages':
-                    if (isset($values['error_codes'])) {
-                        $service->replaceArgument(0, $values['error_codes']);
-                    }
-                    if (isset($values['kernel_root_dir'])) {
-                        $service->replaceArgument(1, $values['kernel_root_dir']);
-                    }
-                    if (isset($values['twig.exception_listener.controller'])) {
-                        $service->replaceArgument(2, $values['twig.exception_listener.controller']);
-                    }
+                    $service->addArgument($values['error_codes']);
+                    $service->addArgument($values['kernel_root_dir']);
+                    $service->addArgument($values['controller']);
                     break;
 
                 case 'symfony_version_check':
                     break;
 
                 case 'deps_entries':
-                    if (isset($values)) {
-                        $service->replaceArgument(0, $values);
+                    if (!is_string($values)) {
+                        $values = '%kernel.root_dir%';
                     }
+                    $service->addArgument($values);
                     break;
 
                 case 'memcache':
-                    if (isset($values['host'])) {
-                        $service->replaceArgument(0, $values['host']);
-                    }
-                    if (isset($values['port'])) {
-                        $service->replaceArgument(1, $values['port']);
-                    }
+                    $service->addArgument($values['host']);
+                    $service->addArgument($values['port']);
                     break;
 
                 case 'php_extensions':
-                    if (isset($values)) {
-                        $service->replaceArgument(0, $values);
-                    }
+                    $service->addArgument($values);
                     break;
 
                 case 'process_active':
-                    if (isset($values)) {
-                        $service->replaceArgument(0, $values);
-                    }
+                    $service->addArgument($values);
                     break;
 
                 case 'writable_directory':
-                    if (isset($values)) {
-                        $service->replaceArgument(0, $values);
-                    }
+                    $service->addArgument($values);
                     break;
 
                 case 'disc_usage':
-                    if (isset($values['percentage'])) {
-                        $service->replaceArgument(0, $values['percentage']);
-                    }
-                    if (isset($values['path'])) {
-                        $service->replaceArgument(1, $values['path']);
-                    }
+                    $service->addArgument($values['percentage']);
+                    $service->addArgument($values['path']);
                     break;
-
-                default:
-                    if (!$container->hasDefinition($check)) {
-                        throw new InvalidConfigurationException("LiipMonitorBundle does not provide a service definition for '$check'");
-                    }
-                    $service = $container->getDefinition($check);
             }
 
             $service->addTag('liip_monitor.check', array('alias' => $check));
