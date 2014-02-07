75538784d1e67f6630419d2cb3d6ba9d7bf31560
Ahmad Jawad <ahmad.jawad@gmail.com>
2014-02-07 20:28:21 +0500

    Added User-Agent Header as required by GitHub API

--- Check/SymfonyVersionCheck.php
+++ Check/SymfonyVersionCheck.php
@@ -46,8 +46,17 @@ class SymfonyVersionCheck extends Check
     {
         // Get GitHub JSON request
 
+        $opts = array(
+ 50                 'http'=>array(
+ 51                 'method'=>"GET",
+ 52                 'header'=>"User-Agent: LiipMonitorBundle\r\n"
+ 53                 )
+ 54         );
+ 55 
+ 56     $context  = stream_context_create($opts);
+
         $githubUrl = 'https://api.github.com/repos/symfony/symfony/tags';
-        $githubJSONResponse = file_get_contents($githubUrl);
+        $githubJSONResponse = file_get_contents($githubUrl, false, $context);
 
         // Convert it to a PHP object
 
