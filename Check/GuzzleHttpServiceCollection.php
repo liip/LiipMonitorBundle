7217fdce011d3c4107cbabcecdf33f399f1168b8
Kevin Bond <kevinbond@gmail.com>
2015-02-05 08:15:31 -0500

    added additional GuzzleHttpService config

--- Check/GuzzleHttpServiceCollection.php
+++ Check/GuzzleHttpServiceCollection.php
@@ -15,7 +15,16 @@ class GuzzleHttpServiceCollection implements CheckCollectionInterface
     public function __construct(array $configs)
     {
         foreach ($configs as $name => $config) {
-            $check = new GuzzleHttpService($config['url'], $config['headers'], $config['options'], $config['status_code'], $config['content']);
+            $check = new GuzzleHttpService(
+                $config['url'],
+                $config['headers'],
+                $config['options'],
+                $config['status_code'],
+                $config['content'],
+                null,
+                $config['method'],
+                $config['body']
+            );
             $check->setLabel(sprintf('Guzzle Http Service "%s"', $name));
 
             $this->checks[sprintf('guzzle_http_service_%s', $name)] = $check;
