3521f6e7e2deecf05d8d8cfde417de017c23c0e2
Lukas Kahwe Smith <smith@pooteeweet.org>
2017-12-01 13:41:42 +0100

    Symfony4 compatibility fixes

--- DependencyInjection/LiipMonitorExtension.php
+++ DependencyInjection/LiipMonitorExtension.php
@@ -49,6 +49,18 @@ class LiipMonitorExtension extends Extension
 
         $container->setParameter(sprintf('%s.default_group', $this->getAlias()), $config['default_group']);
 
+        // symfony3 does not define templating.helper.assets unless php templating is included
+        if ($container->has('templating.helper.assets')) {
+            $pathHelper = $container->getDefinition('liip_monitor.helper');
+            $pathHelper->replaceArgument(0, 'templating.helper.assets');
+        }
+
+        // symfony3 does not define templating.helper.router unless php templating is included
+        if ($container->has('templating.helper.router')) {
+            $pathHelper = $container->getDefinition('liip_monitor.helper');
+            $pathHelper->replaceArgument(1, 'templating.helper.router');
+        }
+
         if (empty($config['checks'])) {
             return;
         }
